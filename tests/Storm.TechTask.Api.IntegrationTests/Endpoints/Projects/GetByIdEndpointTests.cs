929b99f3f43c22076a2b983ff4d0fd500417ab0e
liamc <70069335+LiamCourtney67@users.noreply.github.com>
2026-03-13 11:37:51 +0000

    Task 4: Added tests for GetById endpoint, all pass successfully.

---
+++ tests/Storm.TechTask.Api.IntegrationTests/Endpoints/Projects/GetByIdEndpointTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Duende.IdentityModel.Client;
+
+using Storm.TechTask.Api.Endpoints.Project;
+using Storm.TechTask.Api.IntegrationTests.Utilities;
+using Storm.TechTask.SharedKernel.Authorization;
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Storm.TechTask.Api.IntegrationTests.Endpoints.Projects
+{
+    [Collection("Sequential")]
+    public class GetByIdEndpointTests : BaseEndpointFixture
+    {
+        public GetByIdEndpointTests(ITestOutputHelper output, CustomWebApplicationFactory factory) : base(output, factory)
+        {
+        }
+
+        [Theory]
+        [AllRolesExcept(AppRole.SysAdmin)]
+        public async Task ReturnsProjectById(AppRole role)
+        {
+            // Arrange
+            var project = await NewProject().BuildAndPersist();
+            this.HttpClient.SetBearerToken(await this.TokenIssuer.GetNewToken(role));
+
+            // Act
+            var response = await this.HttpClient.GetAsync($"/Projects/{project.Id}");
+
+            // Assert
+            await response.ShouldBeSuccess().WithObjectPayload(new ProjectDto(project.Id, project.Name));
+        }
+
+        [Fact]
+        public async Task ShouldBeForbidden()
+        {
+            // Arrange
+            var project = await NewProject().BuildAndPersist();
+            this.HttpClient.SetBearerToken(await this.TokenIssuer.GetNewToken(AppRole.SysAdmin));
+
+            // Act
+            var response = await this.HttpClient.GetAsync($"/Projects/{project.Id}");
+
+            // Assert
+            response.ShouldBeNotAuthorised();
+        }
+
+        [Fact]
+        public async Task ShouldBeUnauthorized()
+        {
+            // Arrange
+            var project = await NewProject().BuildAndPersist();
+
+            // Act
+            var response = await this.HttpClient.GetAsync($"/Projects/{project.Id}");
+
+            // Assert
+            response.ShouldBeNotAuthenticated();
+        }
+    }
+}
