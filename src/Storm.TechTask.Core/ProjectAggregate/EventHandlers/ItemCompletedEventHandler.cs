9b00a3b893d8e0e7c445eed71f6d5d5bbe64b23f
liamc <70069335+LiamCourtney67@users.noreply.github.com>
2026-03-15 18:41:54 +0000

    Task 8: Added EventHandlers to log Events (also renamed NewItemAddedEvent)

---
+++ src/Storm.TechTask.Core/ProjectAggregate/EventHandlers/ItemCompletedEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+
+using Storm.TechTask.Core.ProjectAggregate.Events;
+using Storm.TechTask.SharedKernel.Interfaces;
+
+namespace Storm.TechTask.Core.ProjectAggregate.EventHandlers
+{
+    public class ItemCompletedEventHandler : INotificationHandler<ItemCompletedEvent>
+    {
+        private readonly ILoggingService _loggingService;
+
+        public ItemCompletedEventHandler(ILoggingService loggingService)
+        {
+            _loggingService = loggingService;
+        }
+
+        public Task Handle(ItemCompletedEvent notification, CancellationToken cancellationToken)
+        {
+            _loggingService.CommonLogger.Information(
+                "ToDo item completed in project {ProjectId}: {ItemTitle}",
+                notification.Project.Id,
+                notification.ItemCompleted.Title);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Storm.TechTask.Core/ProjectAggregate/EventHandlers/NewItemAddedEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+
+using Storm.TechTask.Core.ProjectAggregate.Events;
+using Storm.TechTask.SharedKernel.Interfaces;
+
+namespace Storm.TechTask.Core.ProjectAggregate.EventHandlers
+{
+    public class NewItemAddedEventHandler : INotificationHandler<NewItemAddedEvent>
+    {
+        private readonly ILoggingService _loggingService;
+
+        public NewItemAddedEventHandler(ILoggingService loggingService)
+        {
+            _loggingService = loggingService;
+        }
+
+        public Task Handle(NewItemAddedEvent notification, CancellationToken cancellationToken)
+        {
+            _loggingService.CommonLogger.Information(
+                "ToDo item added to project {ProjectId}: {ItemTitle}",
+                notification.Project.Id,
+                notification.NewItem.Title);
+
+            return Task.CompletedTask;
+        }
+    }
+}
